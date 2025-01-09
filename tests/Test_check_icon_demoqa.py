86c6997f16357e2df86e3f149245c2f0acf85219
Aleksey_Grebeshkov <grebeshkov1234@gmail.com>
2025-01-09 22:33:19 +0300

    Commit

--- tests/Test_check_icon_demoqa.py
+++ tests/Test_check_icon_demoqa.py
@@ -2,12 +2,13 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 
-driver = webdriver.Chrome()
-driver.get("https://demoqa.com/")
+def test_check_icon():
+    driver = webdriver.Chrome()
+    driver.get("https://demoqa.com/")
 
-# поиск элемента
-icon = driver.find_element(By.CSS_SELECTOR, '#app > header > a')
-if icon is None:
-    print('Элемент не найден')
-else:
-    print('Элемент найден')
+    # поиск элемента
+    icon = driver.find_element(By.CSS_SELECTOR, '#app > header > a')
+    if icon is None:
+        print('Элемент не найден')
+    else:
+        print('Элемент найден')
