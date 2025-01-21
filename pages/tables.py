0a9d380ef96b1251c306ed89031140ae92aba89a
Aleksey_Grebeshkov <grebeshkov1234@gmail.com>
2025-01-22 00:04:56 +0300

    Commit

---
+++ pages/tables.py
@@ -0,0 +1,13 @@
+from pages.base_page import BasePage
+from components.components import WebElement
+
+
+
+class Tables(BasePage):
+
+    def __init__(self, driver):
+        self.base_url = "https://demoqa.com/webtables"
+        super().__init__(driver, self.base_url)
+
+        self.no_data = WebElement(driver, 'div.rt-noData')
+        self.btn_delete_row = WebElement(driver, "//*[contains(text(), 'delete-record')]", 'xpath')
